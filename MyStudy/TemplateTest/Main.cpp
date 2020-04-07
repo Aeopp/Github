85717a80c91e6e3bf46edec34690608b3da02f89
Aeop <irmxj777@gmail.com>
2020-04-07 18:23:37 +0900

    템플릿으로 교체했습니다

--- MyStudy/TemplateTest/Main.cpp
+++ MyStudy/TemplateTest/Main.cpp
@@ -1,18 +1,7 @@
-#include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <numeric>
+#include <iostream> 
+using namespace std;
 
 int main()
 {
-	std::stringstream ss;
-	std::string in = "String with spaces, and embedded \"quotes\" too";
-	std::string out;
-
-	ss << std::quoted(in);
-	std::cout << "read in     [" << in << "]\n"
-		<< "stored as   [" << ss.str() << "]\n";
-
-	ss >> std::quoted(out);
-	std::cout << "written out [" << out << "]\n";
-
-}
+};
---
+++ MyStudy/TemplateTest/myType.cpp
@@ -0,0 +1,1 @@
+#include "myType.h"
