e69d0afba1db9754cf5b095cbeb93646d36345a7
Aeop <53634464+Aeopp@users.noreply.github.com>
2020-05-05 21:46:02 +0900

    Trie 단어 트리 구현
    Trie 단어 트리 구현

---
+++ MyStudy/C++20_Ranges/C++20_Ranges.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <optional>
+#include <valarray>
+#include <list>
+#include <map>
+int main()
+{
+	constexpr auto cartesian{ [=](auto ...xs) constexpr {
+		return [=](auto f) constexpr
+		{
+			(void)std::initializer_list<int>{
+				((void)call_cart(f, xs, xs...), 0)...
+			};
+		};
+	}
+	};
+
+	auto call_cart{ [](auto f, auto x, auto...rest) constexpr {
+		(void)std::initializer_list<int>{
+			((     (x < rest)  ? (void)f(x,rest) : (void)0),0)...
+		};
+		} };
+
+	std::map<int, float> q;
+	
+}
---
+++ MyStudy/Trie/Trie.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <optional>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <vector>
+#include <string>
+
+template<typename _Ty>
+class Trie
+{
+public:
+	template<typename Iter>
+	void insert(Iter First, Iter End)
+	{
+		if (First == End) { return; }
+		Tries[*First].insert(std::next(First), End); 
+	};
+	template<typename _Contain>
+	void insert(const _Contain& container)
+	{
+		insert(std::begin(container), std::end(container)); 
+	}
+	void insert(const std::initializer_list<_Ty>& InitList)
+	{
+		insert(std::begin(InitList), std::end(InitList)); 
+	}
+	void print(std::vector<_Ty>& Vec) const
+	{
+		if(Tries.empty())
+		{
+			std::copy(std::begin(Vec), std::end(Vec),
+				std::ostream_iterator<_Ty>{std::cout, " "});
+			std::cout << '\n';
+		}
+		for(const auto&[First,Second] : Tries )
+		{
+			Vec.push_back(First);
+			Second.print(Vec);
+			Vec.pop_back();
+		}
+	}
+	void print() const
+	{
+		std::vector<_Ty > Vec;
+		print(Vec);
+	}
+	template<typename Iter>
+	std::optional<std::reference_wrapper<const Trie>>
+		subTrie(Iter First, Iter End)const
+	{
+		if (First == End) { return std::ref(*this); }
+		auto found(Tries.find(*First));
+		if (found == std::end(Tries)) { return {}; }
+
+		return found->second.subTrie(std::next(First), End);
+	};
+	template<typename _Contain>
+	auto subTrie(const _Contain& container)
+	{
+		return subTrie(std::begin(container), std::end(container));
+	}
+private:
+	std::map<_Ty, Trie> Tries;
+};
+
+int main()
+{
+	Trie<std::string > Tester;
+	Tester.insert({ "hi","how","are","you" });
+	Tester.insert({"hi","i","am","great","thanks"});
+	
+	Tester.insert({ "what","are","you","doing"});
+	Tester.insert({ "i","am","watching","a","movie" });
+
+	std::cout << "기록된것 전부출력:\n";
+	Tester.print();
+
+	std::cout << "\n \"hi\":\n";
+	if (auto Stance = Tester.subTrie(std::initializer_list<std::string>{"hi"});
+		Stance)
+	
+		Stance->get().print();
+
+
+	
+}
+
+
